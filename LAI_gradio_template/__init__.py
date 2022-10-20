e0caa9fb9b38b3055050d6447dafd088d286276b
William Falcon <williamfalcon@Williams-MacBook-Pro-2.local>
2022-10-20 05:45:40 -0700

    v1

--- LAI_gradio_template/__init__.py
+++
@@ -1,4 +0,0 @@
-from LAI_gradio_template.components.component_a import ComponentA
-from LAI_gradio_template.components.component_b import ComponentB
-
-__all__ = ["ComponentA", "ComponentB"]
--- LAI_gradio_template/components/component_a/__init__.py
+++
@@ -1,3 +0,0 @@
-from LAI_gradio_template.components.component_a.component_a import ComponentA
-
-__all__ = ["ComponentA"]

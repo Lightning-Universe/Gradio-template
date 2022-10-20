817248560385e6de1e69fbe657b090fa5dce7034
William Falcon <williamfalcon@Williams-MacBook-Pro-2.local>
2022-10-20 05:47:14 -0700

    v1

--- LAI_gradio_template/components/component_b/__init__.py
+++
@@ -1,3 +0,0 @@
-from LAI_gradio_template.components.component_b.component_a import ComponentB
-
-__all__ = ["ComponentB"]
--- LAI_gradio_template/components/component_b/component_a.py
+++
@@ -1,6 +0,0 @@
-import lightning as L
-
-
-class ComponentB(L.LightningFlow):
-    def run(self):
-        print("hello from component B")

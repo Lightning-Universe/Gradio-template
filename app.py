b0291f690c97153e0a5b08ae764e261ff774684b
William Falcon <williamfalcon@Williams-MBP-2.lan>
2022-10-21 17:01:41 -0400

    v1

--- app.py
+++ app.py
@@ -10,26 +10,25 @@ from lightning.app.components.serve import ServeGradio
 
 class LitGradio(ServeGradio):
 
-    inputs = gr.inputs.Image(type="pil")
-    outputs = gr.outputs.Image(type="pil")
-    demo_img = "https://cdn.gobankingrates.com/wp-content/uploads/2022/02/shutterstock_editorial_10298578j-1.jpg"
+    inputs = gr.inputs.Image(type="pil", label="Upload Image for Object Detection")
+    outputs = gr.outputs.Image(type="pil", label="Output Image")
+    demo_img = "https://imageio.forbes.com/specials-images/imageserve/60abf319b47a409ca17f4a3f/Pedestrians-cross-Broadway--in-the-SoHo-neighborhood-in-New-York--United-States--May/960x0.jpg?format=jpg&width=960"
     img = Image.open(requests.get(demo_img, stream=True).raw)
-    img.save("shutterstock_editorial_10298578j-1.jpg")
-    examples = [["shutterstock_editorial_10298578j-1.jpg"]]
+    img.save("960x0.jpg")
+    examples = [["960x0.jpg"]]
 
     def __init__(self):
         super().__init__()
         self.ready = False
 
-    def predict(self, img):
-        return self.model(img=img)
-
+    def predict(self, image) -> str:
+        results = self.model(image, size=960)  # includes NMS
+        results.render()
+        return Image.fromarray(results.ims[0])
+    
     def build_model(self):
-        repo = "AK391/animegan2-pytorch:main"
-        model = torch.hub.load(repo, "generator", device="cpu")
-        face2paint = torch.hub.load(repo, "face2paint", size=512, device="cpu")
+        yolo = torch.hub.load("ultralytics/yolov5", "yolov5s")
         self.ready = True
-        return partial(face2paint, model=model)
-
+        return yolo
 
 app = L.LightningApp(LitGradio())
